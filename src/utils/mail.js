b447d81c1b22b9819826df827f1bdc2ad2e87e60
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-06-15 11:15:04 +0530

    feat: update environment variables for Mailtrap and add email utility functions for sending verification and password reset emails

---
+++ src/utils/mail.js
@@ -0,0 +1,78 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmailToUser = async (options) => {
+  const mailGenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "KaryaSetu",
+      link: "https://karyasetu.app",
+    },
+  });
+
+  const htmlMail = mailGenerator.generate(options.mail);
+  const textualMail = mailGenerator.generatePlaintext(options.mail);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_HOST,
+    port: process.env.MAILTRAP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_USERNAME,
+      pass: process.env.MAILTRAP_PASSWORD,
+    },
+  });
+
+  const mail = {
+    from: "karyasetu@gmail.com",
+    to: options.email,
+    subject: options.subject,
+    text: textualMail,
+    html: htmlMail,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+  } catch (error) {
+    console.error("Error sending mail", error);
+  }
+};
+
+export const emailVerificationMailTemplate = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to KaryaSetu! We're excited to have you on board.",
+      action: {
+        instructions:
+          "To verify your email and activate your account, please click the button below.",
+        button: {
+          color: "#22BC66",
+          text: "Verify your account",
+          link: verificationUrl,
+        },
+      },
+      outro:
+        "Need help, or have questions? Just reply to this email, we'd love to help.",
+    },
+  };
+};
+
+export const forgetPasswordMailTemplate = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro:
+        "We received a request to reset the password for your KaryaSetu account.",
+      action: {
+        instructions: "To reset your password, please click the button below.",
+        button: {
+          color: "#FF5A5F",
+          text: "Reset your password",
+          link: passwordResetUrl,
+        },
+      },
+      outro:
+        "If you did not request a password reset, no further action is required and you can safely ignore this email.",
+    },
+  };
+};
