02e0a94572de74398d366bc741e998de5e9200bb
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-06-21 08:32:19 +0530

    feat: add validatePermissions middleware for workspace access control

---
+++ src/middleware/validatePermissions.js
@@ -0,0 +1,32 @@
+import asyncHandler from "../utils/asyncHandler";
+import apiError from "../utils/apiError";
+import WorkspaceMember from "../models/workspaceMember.models";
+
+const validatePermissions = (roles = []) => {
+  return asyncHandler(async (req, res, next) => {
+    const { workspaceId } = req.params;
+    if (!workspaceId) {
+      throw new apiError(400, "Workspace ID is missing");
+    }
+
+    const membership = await WorkspaceMember.findOne({
+      workspaceId: workspaceId,
+      userId: req.user._id,
+    });
+    if (!membership) {
+      throw new apiError(404, "Workspace not found or you are not a member");
+    }
+
+    const currentRole = membership.role;
+    req.workspaceRole = currentRole;
+    if (!allowedRoles.includes(currentRole)) {
+      throw new apiError(
+        403,
+        "You do not have permission to perform this action in this workspace"
+      );
+    }
+    next();
+  });
+};
+
+export default validatePermissions;
