7812c0a7ea255caecb78dd4285d022d83b72fa9e
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-06-25 22:04:59 +0530

    feat: implement listProjectTasks and getTaskById functionalities, enhance task routes with proper authentication and permissions

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -4,6 +4,7 @@ import apiResponse from "../utils/apiResponse.js";
 import Project from "../models/project.models.js";
 import Task from "../models/task.models.js";
 import { uploadOnCloudinary, deleteOnCloudinary } from "../utils/cloudinary.js";
+import ProjectMember from "../models/projectMember.js";
 
 const createTask = asyncHandler(async (req, res) => {
   const { projectId } = req.params;
@@ -49,4 +50,78 @@ const createTask = asyncHandler(async (req, res) => {
     .json(new apiResponse(200, "Task created successfully", createdTask));
 });
 
-export { createTask };
+const listProjectTasks = asyncHandler(async (req, res) => {
+  const { projectId } = req.params;
+
+  const page = Math.max(1, parseInt(req.query.page) || 1);
+  const limit = Math.max(1, parseInt(req.query.limit) || 20);
+  const skip = (page - 1) * limit;
+
+  if (!projectId) throw new apiError(400, "Project Id is missing");
+
+  const [tasks, totalDocument] = await Promise.all([
+    Task.find({ projectId })
+      .sort({ status: -1 })
+      .skip(skip)
+      .limit(limit)
+      .populate("createdBy", "fullName email profile")
+      .lean(),
+
+    Task.countDocuments({ projectId }),
+  ]);
+  const totalPage = Math.ceil(totalDocument / limit);
+  res.status(200).json(
+    new apiResponse(200, "Project Tasks fetched successfully", {
+      tasks,
+      totalDocument,
+      totalPage,
+      currentPage: page,
+      hasNextPage: page < totalPage,
+      hasPreviousPage: page > 1,
+    })
+  );
+});
+
+const getMyTasks = asyncHandler(async (req, res) => {
+  const tasks = await Task.find({
+    assigneeId: req.user._id,
+  })
+    .lean()
+    .populate("createdBy", "name email profile")
+    .populate("projectId", "name");
+
+  if (tasks.length === 0) {
+    return res
+      .status(200)
+      .json(
+        new apiResponse(200, "You have no tasks assigned to you right now.", [])
+      );
+  }
+  res
+    .status(200)
+    .json(new apiResponse(200, "Your tasks fetched successfully", tasks));
+});
+
+const getTaskById = asyncHandler(async (req, res) => {
+  const { taskId } = req.params;
+  if (!taskId) throw new apiError(400, "Task Id is missing");
+
+  const task = await Task.findOne({
+    _id: taskId,
+  })
+    .lean()
+    .populate("createdBy", "name email profile")
+    .populate("projectId", "name");
+
+  if (!task) throw new apiError(404, "Task not found");
+
+  const projectMember = ProjectMember.findOne({
+    userId: req.user._id,
+    projectId: task.projectId,
+  });
+  if (!projectMember)
+    throw new apiError(403, "You are not allowed to view task");
+
+  res.status(200).json(new apiResponse(200, "Task fetched successfully", task));
+});
+export { createTask, listProjectTasks, getMyTasks, getTaskById };
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import { createTask } from "../controllers/task.controller.js";
+import {
+  createTask,
+  listProjectTasks,
+  getMyTasks,
+  getTaskById,
+} from "../controllers/task.controller.js";
 import { createTaskValidation } from "../validators/index.js";
 import userAuth from "../middleware/userAuth.middleware.js";
 import { validateProjectPermissions } from "../middleware/validatePermissions.js";
 import validate from "../middleware/validator.middleware.js";
-import { ProjectRoleEnum } from "../constant.js";
+import { AvailableProjectRoles, ProjectRoleEnum } from "../constant.js";
 import { uploadTaskNotes } from "../middleware/multer.middleware.js";
 const router = Router();
 router
@@ -20,4 +25,17 @@ router
     validate,
     createTask
   );
+
+router
+  .route("/:projectId/tasks")
+  .get(
+    userAuth,
+    validateProjectPermissions(AvailableProjectRoles),
+    listProjectTasks
+  );
+
+router.route("/my-tasks").get(userAuth, getMyTasks);
+
+router.route("/:taskId").get(userAuth, getTaskById);
+
 export default router;
